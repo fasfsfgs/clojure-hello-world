c65dcc5e84aadab590cf59741b7f0e81e65f1372
fasfsfgs <fasfsfgs@gmail.com>
2015-09-07 15:42:11 -0300

    Using lein ring for dev; Accessing db;

--- src/app/core.clj
+++ src/app/core.clj
@@ -1,10 +1,13 @@
 (ns app.core
   (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
-            [compojure.handler :refer [site]]
             [compojure.route :as route]
             [clojure.java.io :as io]
             [ring.adapter.jetty :as jetty]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+            [ring.middleware.nested-params :refer [wrap-nested-params]]
             [camel-snake-kebab.core :as kebab]
+            [clojure.java.jdbc :as db]
             [environ.core :refer [env]]))
 
 (def sample (env :sample "sample-string-thing"))
@@ -12,21 +15,32 @@
 (defn splash []
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body (for [kind ["camel" "snake" "kebab"]]
-           (format "<a href=\"/%s?input=%s\">%s %s</a><br />"
-                   kind sample kind sample))})
+   :body (concat (for [kind ["camel" "snake" "kebab"]]
+                   (format "<a href=\"/%s?input=%s\">%s %s</a><br />"
+                           kind sample kind sample))
+                 ["<hr /><ul>"]
+                 (for [s (db/query (env :database-url "postgres://localhost:5432/testdb")
+                                   ["select content from sayings"])]
+                   (format "<li>%s</li>" (:content s)))
+                 ["</ul>"])})
 
-(defroutes app
+(defn record [input]
+  (db/insert! (env :database-url "postgres://localhost:5432/testdb")
+              :sayings {:content input}))
+
+(defroutes appRoutes
   (GET "/camel" {{input :input} :params}
+       (record input)
        {:status 200
         :headers {"Content-Type" "text/plain"}
         :body (kebab/->camelCase input)})
   (GET "/snake" {{input :input} :params}
-       (print-str input)
+       (record input)
        {:status 200
         :headers {"Content-Type" "text/plain"}
         :body (kebab/->snake_case input)})
   (GET "/kebab" {{input :input} :params}
+       (record input)
        {:status 200
         :headers {"Content-Type" "text/plain"}
         :body (kebab/->kebab-case input)})
@@ -35,10 +49,11 @@
   (ANY "*" []
        (route/not-found (slurp (io/resource "404.html")))))
 
+(def app
+  (-> appRoutes
+      (wrap-keyword-params)
+      (wrap-params)))
+
 (defn -main [& [port]]
   (let [port (Integer. (or port (env :port 5000)))]
-    (jetty/run-jetty (site #'app) {:port port :join? false})))
-
-;; For interactive development:
-;; (.stop server)
-;; (def server (-main))
+    (jetty/run-jetty app {:port port :join? false})))
